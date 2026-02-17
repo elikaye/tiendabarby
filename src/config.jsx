2cc8c4279cb0eddc98d66b2ae33b47fbe05ecd12
elikaye <elianakaye13@gmail.com>
2026-02-17 16:54:03 -0300

    fix: limpieza de variables de entorno y config API

--- src/config.jsx
+++ src/config.jsx
@@ -1,16 +1,6 @@
-// config.js
 
-// ========================
-// API BASE
-// ========================
-export const API_BASE_URL =
-  import.meta.env.MODE === "development"
-    ? `${import.meta.env.VITE_BACKEND_URL_DEV}/api/v1`
-    : `${import.meta.env.VITE_BACKEND_URL}/api/v1`;
+export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
-// ========================
-// CLOUDINARY
-// ========================
 export const CLOUDINARY_BASE_URL =
   import.meta.env.VITE_CLOUDINARY_BASE_URL;
 
